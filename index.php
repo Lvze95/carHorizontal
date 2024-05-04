8da7f949e57c88d0c34a04eb60f8b0ec60368228
velimirovicl <lazar.velimirovic@gmail.com>
2024-05-04 14:00:35 +0200

    Implementacija funkcionalnosti dodavanja u bazu

--- index.php
+++ index.php
@@ -39,6 +39,55 @@
         
     </form>
     <div class="kontent" id="php">
+    <?php
+    
+
+    class Automobil {
+        public $broj_Sasije;
+        public $model;
+        public $godiste;
+        public $kilometraza;
+        public $vlasnik;
+    }
+    try{
+        $pdo = new PDO("mysql:host=localhost;dbname=carhorizontal", "root", "");
+        
+        
+        $sql = "INSERT INTO automobil(brSasije, model, godiste, kilometraza, vlasnik)
+        VALUES(:brSasije, :model, :godiste, :kilometraza, :vlasnik)";
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(":brSasije", $brSasije);
+        $stmt->bindParam(":model", $model);
+        $stmt->bindParam(":godiste", $godiste);
+        $stmt->bindParam(":kilometraza", $kilometraza);
+        $stmt->bindParam(":vlasnik", $vlasnik);
+
+        if(empty($_POST['brSasije']) or empty($_POST['model']) or empty($_POST['godiste']) or empty($_POST['kilometraza']) or empty($_POST['vlasnik'])){
+            echo "Sva polja moraju biti popunjena";
+        }
+        else if(ctype_alpha($vlasnik = $_POST['vlasnik'])){
+            $automobil = new Automobil();
+            $brSasije = $_POST['brSasije'];
+            $model = $_POST['model'];
+            $godiste = $_POST['godiste'];
+            $kilometraza = $_POST['kilometraza'];
+            $vlasnik = $_POST['vlasnik'];    
+            $stmt->execute();
+            echo "<h3 style='color:#73bd1b'>Uspesno uneti podaci</h3>";
+        }
+        else{
+            echo "Polje vlasnik mora sadrzati samo slova!";
+        } 
+
+
+    }
+    catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+    ?>
     </div>
 
     <select name="model">
