6a024ea5e292d9a9d46428cee0988ff1a45add71
Milan <nikolic.milan1707@gmail.com>
2024-05-04 12:52:33 +0200

    Implementacija pretrage u bazi

--- pretraga.php
+++ pretraga.php
@@ -3,9 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <title>Pretraga</title>
 </head>
 <body>
     
+<div class="hero">
+        <img src="logo.png" alt="log" id="logo">
+        <h1> Spisak vozila </h1>
+    </div>
+    <div class="kontent" id="php">
+    <?php
+
+    try{
+        $pdo = new PDO("mysql:host=localhost;dbname=carhorizontal", "root", "");
+        
+        $sql1 = "DROP PROCEDURE IF EXISTS podaci_auta";
+        $sql2 = "CREATE PROCEDURE podaci_auta()
+        BEGIN
+            SELECT * FROM automobil;
+        END;
+        ";
+        $pdo->exec($sql1);
+        $pdo->exec($sql2);
+
+        $sql = "CALL podaci_auta()";
+        $result = $pdo->query($sql);
+            foreach($result as $row){
+                echo "Broj sasije: " . $row['brSasije'] . "<br>" .
+                "Model auta: <i>" . $row['model'] . "</i> <br>" . 
+                "Godiste auta: " . $row['godiste'] . "<br>" . 
+                "Kilometraza auta: " . $row['kilometraza'] . " <br>" .
+                "Vlasnik auta: " . $row['vlasnik'] . "<br><br>";
+            }
+
+    }
+    catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+    ?>
+    </div>
+
 </body>
 </html>
